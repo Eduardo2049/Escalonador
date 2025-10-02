40f52877a2b4076d1579ba306db951ed228158d2
Eduardo <eduardofelipes100@gmail.com>
2025-10-02 15:13:11 -0300

    adição de criação de escalonador e struct

--- escalonador.c
+++ escalonador.c
@@ -14,3 +14,25 @@ typedef struct Fila {
     PCB* inicio;
     PCB* fim;
 } Fila;
+
+#define Num_fila 32
+Fila escalonador[Num_fila];
+
+void new_escalonador(){
+    for(int i=0; I< Num_fila; i++){
+        escalonador[i].inicio = null;
+        escalonador[i].fim = null;
+    }
+}
+
+int main () {
+    new_escalonador();
+    unsigned int pprid = 0;
+    for (int i=0; i < 1000000; i++){
+        pprid++;
+        unsigned int fx = (rand() * 1000) & 0x1f;
+
+        PCB* novo = criar_novo(pprid, fila)
+    }
+    
+}
