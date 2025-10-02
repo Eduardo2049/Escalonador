e1be4f3cd522c3e0200abee95e7a0bf8875f0eb5
Eduardo <eduardofelipes100@gmail.com>
2025-10-02 16:33:10 -0300

    adição de while para combater processos de fila baixa prioridade

--- escalonador.c
+++ escalonador.c
@@ -15,11 +15,11 @@ typedef struct Fila {
     PCB* fim;
 } Fila;
 
-#define Num_fila 32
-Fila escalonador[Num_fila];
+#define NUM_FILAS 32
+Fila escalonador[NUM_FILAS];
 
 void new_escalonador(){
-    for(int i=0; I< Num_fila; i++){
+    for(int i=0; I< NUM_FILAS; i++){
         escalonador[i].inicio = null;
         escalonador[i].fim = null;
     }
@@ -32,7 +32,19 @@ int main () {
         pprid++;
         unsigned int fx = (rand() * 1000) & 0x1f;
 
-        PCB* novo = criar_novo(pprid, fila)
+        PCB* novo = criar_novo(pprid, fila_aleatoria);
+        add_fila(&escalonador[fila_aleatoria], novo);
     }
     
+    while (1){
+        if (ciclo>0 && ciclo %10 == 0) {
+            for (int i = NUM_FILAS -1; i > 0; i--){
+                PCB* p = remover_inicio(&escalonador[i]);
+                if(p != NULL) {
+                    p-> fila = i-1;
+                    add_fila(&escalonador[i-1], p);
+                }
+            }
+        }
+    }
 }
